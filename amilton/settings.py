6dea0bd1d1617d88c3b85dbd40229a19f603d00d
unknown <amilton0656@gmail.com>
2025-07-15 21:23:44 -0300

    inicio

--- amilton/settings.py
+++ amilton/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -16,6 +17,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+APPS_DIR = str(os.path.join(BASE_DIR,'apps')) # Dentro da pasta apps na raiz do projeto
+sys.path.insert(0, APPS_DIR)
+
 
 # Application definition
 
@@ -26,7 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    'apps.core',
+    'apps.friends',
 ]
 
 MIDDLEWARE = [
